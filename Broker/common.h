dc0fdf3eb22e6596f6ea24c8426885eb83a32947
chris <hugsy@blah.cat>
2019-09-10 17:56:37 -0700

    few changes

--- Broker/common.h
+++ Broker/common.h
@@ -5,5 +5,6 @@
 extern "C"
 {
 #include "../common/Common.h"
+#include "../Driver/IoctlCodes.h"
 }
 
--- Broker/task.h
+++ Broker/task.h
@@ -4,16 +4,19 @@
 
 #include <string>
 
-enum TaskType : short
+enum TaskType : uint32_t
 {
+	TaskTypeMin = 0,
+	IoctlResponse,
 	HookDriver,
 	UnhookDriver,
 	GetDriverInfo,
-	GetNumberOfDriver
+	GetNumberOfDriver,
+	TaskTypeMax
 };
 
 
-enum TaskState : short
+enum TaskState : uint16_t
 {
 	Initialized,
 	Queued,
@@ -26,14 +29,20 @@ enum TaskState : short
 class Task
 {
 public:
-	
-	Task(TaskType type, byte* data, size_t datalen)
+
+	Task(TaskType type, byte* data, uint32_t datalen, uint32_t code)
 	{
 		_type = type;
 		_state = Initialized;
+		_code = code;
+		_data_length = datalen;
 		_data = new byte[datalen];
 		::memcpy(_data, data, datalen);
 	}
+
+	Task(TaskType type, byte* data, uint32_t datalen)
+		: Task(type, data, datalen, 0)
+	{ }
 
 	~Task()
 	{
@@ -52,7 +61,7 @@ public:
 		return L"??";
 	}
 
-	std::wstring Type()
+	std::wstring TypeAsString()
 	{
 		switch (_type)
 		{
@@ -64,14 +73,43 @@ public:
 		return L"(Unknown)";
 	}
 
+	TaskType Type() 
+	{ 
+		return _type; 
+	}
+
+	DWORD IoctlCode()
+	{
+		switch (_type)
+		{
+		case HookDriver: return IOCTL_AddDriver;
+		case UnhookDriver: return IOCTL_RemoveDriver;
+		}
+
+		return ERROR_BAD_ARGUMENTS;
+	}
+
 	void SetState(TaskState s)
 	{
 		_state = s;
 	}
+
+	uint32_t Length()
+	{
+		return _data_length;
+	}
+
+	byte* Data()
+	{
+		return _data;
+	}
+
 
 
 private:
 	TaskType _type;
 	TaskState _state;
 	byte* _data = NULL;
+	uint32_t _data_length;
+	DWORD _code;
 };
