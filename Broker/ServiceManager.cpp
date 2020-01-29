hugsy/CFB

commit 379c50f41d829cce8b147e2ede8d64d1b4d4f933

fixed issue #13: cfb can be run as a win32 service

Broker/ServiceManager.cpp
@@ -1,4 +1,7 @@
-#include "ServiceManager.h"
+#include "main.h"
+
+extern Session* Sess;
+
 
 /*++
 
@@ -17,6 +20,68 @@ defines all the functions to properly:
 
 
 
+
+/*++
+
+Routine Description:
+
+Creates the service manager object.
+
+
+Arguments:
+
+	None
+
+
+Return Value:
+	Nothing, throw an exception on any error
+
+--*/
+ServiceManager::ServiceManager()
+{
+	if (!ExtractDriverFromResource())
+		RAISE_GENERIC_EXCEPTION("ExtractDriverFromResource() failed");
+
+	bIsDriverExtracted = TRUE;
+
+	if (!LoadDriver())
+		RAISE_GENERIC_EXCEPTION("LoadDriver() failed");
+
+	bIsDriverLoaded = TRUE;
+}
+
+
+
+/*++
+
+Routine Description:
+
+Destroy the service manager object.
+
+
+Arguments:
+
+	None
+
+
+Return Value:
+	Nothing, throw an exception on any error
+	
+--*/
+ServiceManager::~ServiceManager() noexcept(false)
+{
+	if (bIsDriverLoaded)
+	{
+		if (!UnloadDriver())
+			RAISE_GENERIC_EXCEPTION("UnloadDriver() failed");
+	}
+
+	if (bIsDriverExtracted)
+	{
+		if (!DeleteDriverFromDisk())
+			RAISE_GENERIC_EXCEPTION("DeleteDriverFromDisk() failed");
+	}
+}
 
 
 /*++
@@ -82,13 +147,13 @@ BOOL ServiceManager::ExtractDriverFromResource()
 	}
 
 	DWORD dwWritten;
-	if (!WriteFile(hDriverFile, hgDriverRsc, dwDriverSize, &dwWritten, NULL))
+	if (!::WriteFile(hDriverFile, hgDriverRsc, dwDriverSize, &dwWritten, NULL))
 	{
 		PrintErrorWithFunctionName(L"WriteFile()");
 		retcode = FALSE;
 	}
 
-	CloseHandle(hDriverFile);
+	::CloseHandle(hDriverFile);
 	return retcode;
 }
 
@@ -117,54 +182,6 @@ BOOL ServiceManager::DeleteDriverFromDisk()
 	return DeleteFile(lpszFilePath);
 }
 
-
-/*++
-
-Routine Description:
-
-Creates the service manager object.
-
-
-Arguments:
-
-	None
-
-
-Return Value:
-	Nothing, throw an exception on any error
-
---*/
-ServiceManager::ServiceManager()
-{
-	if (!LoadDriver())
-		RAISE_GENERIC_EXCEPTION("LoadDriver() failed");
-
-}
-
-
-
-/*++
-
-Routine Description:
-
-Destroy the service manager object.
-
-
-Arguments:
-
-	None
-
-
-Return Value:
-	Nothing, throw an exception on any error
-	
---*/
-ServiceManager::~ServiceManager() noexcept(false)
-{
-	if (!UnloadDriver())
-		RAISE_GENERIC_EXCEPTION("UnloadDriver() failed");
-
-}
 
 
 /*++
@@ -277,12 +294,12 @@ Return Value:
 --*/
 BOOL ServiceManager::UnloadDriver()
 {
-	SERVICE_STATUS ServiceStatus;
+	SERVICE_STATUS DriverServiceStatus;
 
 #ifdef _DEBUG
 	xlog(LOG_DEBUG, L"Stopping service '%s'\n", CFB_SERVICE_NAME);
 #endif
-	if (!ControlService(hService, SERVICE_CONTROL_STOP, &ServiceStatus))
+	if (!::ControlService(hService, SERVICE_CONTROL_STOP, &DriverServiceStatus))
 	{
 		PrintErrorWithFunctionName(L"ControlService");
 		return FALSE;
@@ -291,15 +308,207 @@ BOOL ServiceManager::UnloadDriver()
 #ifdef _DEBUG
 	xlog(LOG_DEBUG, L"Service '%s' stopped\n", CFB_SERVICE_NAME);
 #endif
-	if (!DeleteService(hService))
+	if (!::DeleteService(hService))
 	{
 		PrintErrorWithFunctionName(L"DeleteService");
 		return FALSE;
 	}
 
 
-	CloseServiceHandle(hService);
-	CloseServiceHandle(hSCManager);
+	::CloseServiceHandle(hService);
+	::CloseServiceHandle(hSCManager);
 
 	return TRUE;
+}
+
+
+
+static VOID ServiceCtrlHandler(DWORD dwCtrlCode)
+{
+	ServiceManager& ServiceManager = Sess->ServiceManager;
+
+	switch (dwCtrlCode)
+	{
+	case SERVICE_CONTROL_CONTINUE:
+	case SERVICE_CONTROL_INTERROGATE:
+	case SERVICE_CONTROL_PAUSE:
+	case SERVICE_CONTROL_SHUTDOWN:
+		xlog(LOG_ERROR, L"Unhandled control code: 0x%x\n", dwCtrlCode);
+		break;
+
+	case SERVICE_CONTROL_STOP:
+
+		if (ServiceManager.m_ServiceStatus.dwCurrentState != SERVICE_RUNNING)
+			break;
+
+		/*
+		 * Perform tasks necessary to stop the service here
+		 */
+
+		ServiceManager.m_ServiceStatus.dwControlsAccepted = 0;
+		ServiceManager.m_ServiceStatus.dwCurrentState = SERVICE_STOP_PENDING;
+		ServiceManager.m_ServiceStatus.dwWin32ExitCode = 0;
+		ServiceManager.m_ServiceStatus.dwCheckPoint = 4;
+
+		if (::SetServiceStatus(ServiceManager.m_StatusHandle, &ServiceManager.m_ServiceStatus) == FALSE)
+		{
+			xlog(LOG_DEBUG, L"SetServiceStatus() failed");
+			break;
+		}
+
+		// This will signal the worker thread to start shutting down
+		Sess->Stop();
+		::SetEvent(ServiceManager.m_ServiceStopEvent);
+		break;
+
+	default:
+		break;
+	}
+
+	return;
+}
+
+
+/*++
+
+Routine Description:
+
+Static routine to initialize the own process service. 
+
+
+Arguments:
+
+	argc - 
+
+	argv - 
+
+
+Return Value:
+
+	None
+
+--*/
+static VOID ServiceMain(DWORD argc, LPWSTR* argv)
+{
+	ServiceManager& ServiceManager = Sess->ServiceManager;
+
+	do
+	{
+
+		ServiceManager.m_StatusHandle = ::RegisterServiceCtrlHandler(WIN32_SERVICE_NAME, ServiceCtrlHandler);
+		if (!ServiceManager.m_StatusHandle)
+		{
+			xlog(LOG_ERROR, L"RegisterServiceCtrlHandler() failed\n");
+			break;
+		}
+
+		::ZeroMemory(&ServiceManager.m_ServiceStatus, sizeof(ServiceManager.m_ServiceStatus));
+		ServiceManager.m_ServiceStatus.dwServiceType = SERVICE_WIN32_OWN_PROCESS;
+		ServiceManager.m_ServiceStatus.dwControlsAccepted = 0;
+		ServiceManager.m_ServiceStatus.dwCurrentState = SERVICE_START_PENDING;
+		ServiceManager.m_ServiceStatus.dwWin32ExitCode = 0;
+		ServiceManager.m_ServiceStatus.dwServiceSpecificExitCode = 0;
+		ServiceManager.m_ServiceStatus.dwCheckPoint = 0;
+
+		if (::SetServiceStatus(ServiceManager.m_StatusHandle, &ServiceManager.m_ServiceStatus) == FALSE)
+		{
+			xlog(LOG_ERROR, L"SetServiceStatus() failed\n");
+			break;
+		}
+
+
+		ServiceManager.m_ServiceStopEvent = ::CreateEvent(NULL, TRUE, FALSE, NULL);
+		if (ServiceManager.m_ServiceStopEvent == NULL)
+		{
+			ServiceManager.m_ServiceStatus.dwControlsAccepted = 0;
+			ServiceManager.m_ServiceStatus.dwCurrentState = SERVICE_STOPPED;
+			ServiceManager.m_ServiceStatus.dwWin32ExitCode = ::GetLastError();
+			ServiceManager.m_ServiceStatus.dwCheckPoint = 1;
+
+			if (::SetServiceStatus(ServiceManager.m_StatusHandle, &ServiceManager.m_ServiceStatus) == FALSE)
+			{
+				xlog(LOG_ERROR, L"SetServiceStatus() failed\n");
+			}
+			break;
+		}
+
+
+		ServiceManager.m_ServiceStatus.dwControlsAccepted = SERVICE_ACCEPT_STOP;
+		ServiceManager.m_ServiceStatus.dwCurrentState = SERVICE_RUNNING;
+		ServiceManager.m_ServiceStatus.dwWin32ExitCode = 0;
+		ServiceManager.m_ServiceStatus.dwCheckPoint = 0;
+
+		if (::SetServiceStatus(ServiceManager.m_StatusHandle, &ServiceManager.m_ServiceStatus) == FALSE)
+		{
+			xlog(LOG_ERROR, L"SetServiceStatus() failed\n");
+			break;
+		}
+
+		dbg(L"CFB background service ready, starting thread...\n");
+
+
+		//
+		// Let's start CFB in background
+		//
+
+		HANDLE hThread = ::CreateThread(NULL, 0, RunForever, NULL, 0, NULL);
+		if (!hThread)
+		{
+			xlog(LOG_ERROR, L"Failed to start the RunForever() thread\n");
+			break;
+		}
+
+		::WaitForSingleObject(hThread, INFINITE);
+		
+		::CloseHandle(ServiceManager.m_ServiceStopEvent);
+
+		//
+		// Propagate the stop info to the service controller
+		//
+		ServiceManager.m_ServiceStatus.dwControlsAccepted = 0;
+		ServiceManager.m_ServiceStatus.dwCurrentState = SERVICE_STOPPED;
+		ServiceManager.m_ServiceStatus.dwWin32ExitCode = 0;
+		ServiceManager.m_ServiceStatus.dwCheckPoint = 3;
+
+		if (::SetServiceStatus(ServiceManager.m_StatusHandle, &ServiceManager.m_ServiceStatus) == FALSE)
+		{
+			xlog(LOG_ERROR, L"SetServiceStatus() failed\n");
+		}
+
+	} 
+	while (0);
+
+
+	return;
+}
+
+
+/*++
+
+Routine Description:
+
+Static routine to register the own process service.
+
+
+Arguments:
+
+	None
+
+
+Return Value:
+
+	None
+
+--*/
+BOOL ServiceManager::RegisterService()
+{
+	auto lpswServiceName = (LPWSTR)WIN32_SERVICE_NAME;
+
+	SERVICE_TABLE_ENTRY ServiceTable[] =
+	{
+		{lpswServiceName, (LPSERVICE_MAIN_FUNCTION)ServiceMain},
+		{NULL, NULL}
+	};
+
+	return ::StartServiceCtrlDispatcher(ServiceTable);
 }

Broker/main.cc
@@ -22,8 +22,9 @@ for communicating with the IrpDumper driver by:
 #include "main.h"
 
 
-static HANDLE g_hTerminationEvent;
+Session* Sess;
 
+static HANDLE g_hTerminationEvent;
 
 /*++
 
@@ -198,107 +199,26 @@ static BOOL CtrlHandler(DWORD fdwCtrlType)
 
 Routine Description:
 
-The entrypoint for the broker.
+Run forever loop, can be run from either the standalone mode or own process service.
 
 
 Arguments:
 
-	argc - 
-
-	argv - 
+	lpParameter - unused parameter, here only for WINAPI compatibility.
 
 
 Return Value:
 
-	Returns EXIT_SUCCESS on success, EXIT_FAILURE
+	Return 0 on success, or the last error code.
 
 --*/
-int wmain(int argc, wchar_t** argv)
+DWORD RunForever(LPVOID lpParameter)
 {
-	int retcode = EXIT_SUCCESS;
-	HANDLE hDriver = INVALID_HANDLE_VALUE;
-	HANDLE hGui = INVALID_HANDLE_VALUE;
+	UNREFERENCED_PARAMETER(lpParameter);
+
+	DWORD retcode = ERROR_SUCCESS;
 	HANDLE ThreadHandles[3] = { 0 };
 	DWORD dwWaitResult = 0;
-	BOOL bHasDebugPriv = FALSE;
-	BOOL bHasLoadDriverPriv = FALSE;
-	Sess = nullptr;
-
-
-	HANDLE hStdErr = GetStdHandle(STD_ERROR_HANDLE);
-	DWORD dwConsoleMode;
-
-	::GetConsoleMode(hStdErr, &dwConsoleMode);
-	dwConsoleMode |= ENABLE_VIRTUAL_TERMINAL_PROCESSING;
-	::SetConsoleMode(hStdErr, dwConsoleMode);
-
-
-	xlog(LOG_INFO, L"Starting %s (part of %s (v%.02f) - by <%s>)\n", argv[0], CFB_PROGRAM_NAME_SHORT, CFB_VERSION, CFB_AUTHOR);
-
-#ifdef _DEBUG
-	xlog(LOG_DEBUG, L"DEBUG mode on\n");
-#endif
-
-
-	//
-	// Check the privileges
-	//
-	const wchar_t* lpszPrivilegeNames[2] = { L"SeDebugPrivilege", L"SeLoadDriverPrivilege" };
-
-	for (int i = 0; i < _countof(lpszPrivilegeNames); i++)
-	{
-		BOOL fHasPriv = FALSE;
-		if (!AssignPrivilegeToSelf(lpszPrivilegeNames[i]))
-		{
-			xlog(LOG_CRITICAL, L"%s requires '%s', cannot proceed...\n", argv[0], lpszPrivilegeNames[i]);
-			return EXIT_FAILURE;
-		}
-	}
-
-	dbg(L"Privilege check succeeded...\n");
-
-
-	//
-	// Extract the IrpDumper driver from the resources
-	//
-	if (!ServiceManager::ExtractDriverFromResource())
-	{
-		xlog(LOG_CRITICAL, L"Failed to extract driver from resource, aborting...\n");
-		return EXIT_FAILURE;
-	}
-
-	dbg(L"Driver extracted...\n");
-
-
-	//
-	// The session is ready to be initialized
-	//
-
-	dbg(L"Initializing the session...\n");
-	
-	try
-	{
-		Sess = new Session();
-	}
-	catch (std::runtime_error& e)
-	{
-		xlog(LOG_CRITICAL, L"Failed to initialize the session, reason: %S\n", e.what());
-		ServiceManager::DeleteDriverFromDisk();
-		return EXIT_FAILURE;
-	}
-
-
-	//
-	// Install the Ctrl-C handler to clean up properly
-	//
-	if (!SetConsoleCtrlHandler((PHANDLER_ROUTINE)CtrlHandler, TRUE))
-	{
-		xlog(LOG_CRITICAL, L"Could not setup SetConsoleCtrlHandler()...\n");
-		delete Sess;
-		ServiceManager::DeleteDriverFromDisk();
-		return EXIT_FAILURE;
-	}
-	
 
 
 	//
@@ -307,22 +227,29 @@ int wmain(int argc, wchar_t** argv)
 
 	if (!StartBackendManagerThread(Sess))
 	{
-		retcode = EXIT_FAILURE;
-		goto __DestroyEnvironment;
+		xlog(LOG_CRITICAL, L"StartBackendManagerThread() failed\n");
+		return retcode;
 	}
 
-	
+
 	//
 	// Initialize the gui <-> broker thread
 	//
 
 	if (!FrontendThreadRoutine(Sess))
 	{
-		retcode = EXIT_FAILURE;
-		goto __DestroyEnvironment;
+		xlog(LOG_CRITICAL, L"FrontendThreadRoutine() failed\n");
+		return retcode;
 	}
 
-	xlog(LOG_SUCCESS, L"ThreadIds[]=[Frontend=%d,Backend=%d,IrpFetcher=%d]\n", GetThreadId(Sess->m_hFrontendThread), GetThreadId(Sess->m_hBackendThread), GetThreadId(Sess->m_hIrpFetcherThread));
+
+	xlog(
+		LOG_SUCCESS, 
+		L"ThreadIds[]=[Frontend=%d,Backend=%d,IrpFetcher=%d]\n", 
+		::GetThreadId(Sess->m_hFrontendThread), 
+		::GetThreadId(Sess->m_hBackendThread), 
+		::GetThreadId(Sess->m_hIrpFetcherThread)
+	);
 
 
 	//
@@ -347,35 +274,156 @@ int wmain(int argc, wchar_t** argv)
 	{
 	case WAIT_OBJECT_0:
 		xlog(LOG_SUCCESS, L"All threads ended, cleaning up...\n");
+		retcode = ERROR_SUCCESS;
 		break;
 
 	default:
 		PrintErrorWithFunctionName(L"WaitForMultipleObjects");
-		retcode = EXIT_FAILURE;
+		retcode = ::GetLastError();
 		break;
 	}
 
+	return retcode;
+}
 
-__DestroyEnvironment:
+
+/*++
+
+Routine Description:
+
+The entrypoint for the broker.
+
+
+Arguments:
+
+	argc - 
+
+	argv - 
+
+
+Return Value:
+
+	Returns EXIT_SUCCESS on success, EXIT_FAILURE
+
+--*/
+int wmain(int argc, wchar_t** argv)
+{
+	int retcode = EXIT_SUCCESS;
+	HANDLE hDriver = INVALID_HANDLE_VALUE;
+	HANDLE hGui = INVALID_HANDLE_VALUE;
+	
+	
+	BOOL bHasDebugPriv = FALSE;
+	BOOL bHasLoadDriverPriv = FALSE;
+	
+	Sess = nullptr;
+
+
+	HANDLE hStdErr = GetStdHandle(STD_ERROR_HANDLE);
+	DWORD dwConsoleMode;
+
+	::GetConsoleMode(hStdErr, &dwConsoleMode);
+	dwConsoleMode |= ENABLE_VIRTUAL_TERMINAL_PROCESSING;
+	::SetConsoleMode(hStdErr, dwConsoleMode);
+
+
+	xlog(LOG_INFO, L"Starting %s (part of %s (v%.02f) - by <%s>)\n", argv[0], CFB_PROGRAM_NAME_SHORT, CFB_VERSION, CFB_AUTHOR);
+
+#ifdef _DEBUG
+	xlog(LOG_DEBUG, L"DEBUG mode on\n");
+#endif
+
+
+	//
+	// Check the privileges first, if we don't have them there is no point going further
+	//
+	const wchar_t* lpszPrivilegeNames[2] = { L"SeDebugPrivilege", L"SeLoadDriverPrivilege" };
+
+	for (int i = 0; i < _countof(lpszPrivilegeNames); i++)
+	{
+		BOOL fHasPriv = FALSE;
+		if (!AssignPrivilegeToSelf(lpszPrivilegeNames[i]))
+		{
+			xlog(LOG_CRITICAL, L"%s requires '%s', cannot proceed...\n", argv[0], lpszPrivilegeNames[i]);
+			return EXIT_FAILURE;
+		}
+	}
+
+	dbg(L"Privilege check succeeded...\n");
+
+
+	//
+	// The session is ready to be initialized
+	//
+
+	dbg(L"Initializing the session...\n");
+	
+	try
+	{
+		Sess = new Session();
+	}
+	catch (std::runtime_error& e)
+	{
+		xlog(LOG_CRITICAL, L"Failed to initialize the session, reason: %S\n", e.what());
+		return EXIT_FAILURE;
+	}
+
+	do
+	{
+
+		//
+		// Should we run as a background service
+		//
+		if (argc > 1 && !wcscmp(argv[1], L"--service"))
+		{
+			Sess->ServiceManager.bRunInBackground = TRUE;
+			if (!Sess->ServiceManager.RegisterService())
+			{
+				xlog(LOG_CRITICAL, L"Failed to register service...\n");
+				retcode = EXIT_FAILURE;
+				break;
+			}
+		}
+
+
+		if (Sess->ServiceManager.bRunInBackground == FALSE)
+		{
+			if (!SetConsoleCtrlHandler((PHANDLER_ROUTINE)CtrlHandler, TRUE))
+			{
+				xlog(LOG_CRITICAL, L"Could not setup SetConsoleCtrlHandler()...\n");
+				retcode = EXIT_FAILURE;
+				break;
+			}
+		}
+
+
+		if (RunForever(NULL) != ERROR_SUCCESS)
+		{
+			xlog(LOG_ERROR, L"RunForever() returned with error\n");
+			break;
+		}
+
+		dbg(L"RunForever() finished successfully\n");
+	} 
+	while (0);
+
+
 	//
 	// Deleting the session will automatically destroy the FrontEnd/BackEnd server, along with 
 	// the service manager.
 	//
-	delete Sess;
-
-
-	//
-	// Cleanup
-	//
-	if (!ServiceManager::DeleteDriverFromDisk())
+	try
 	{
-		xlog(LOG_ERROR, L"An error occured while deleting driver from disk\n");
+		if (Sess != nullptr)
+		{
+			delete Sess;
+		}
+	}
+	catch (std::runtime_error & e)
+	{
+		xlog(LOG_CRITICAL, L"An error occured while deleting the session: %S\n", e.what());
 		retcode = EXIT_FAILURE;
 	}
-#ifdef _DEBUG
-	else
-		dbg(L"Driver deleted\n");
-#endif
 
 
 #ifdef _DEBUG

Broker/ServiceManager.h
@@ -6,6 +6,7 @@
 
 
 #define CFB_DRIVER_LOCATION_DIRECTORY L"C:\\Windows\\System32\\Drivers"
+#define WIN32_SERVICE_NAME L"CFB_Broker"
 
 #define GetDriverOnDiskFullPath(x){\
  	wcscat_s(x, MAX_PATH, CFB_DRIVER_LOCATION_DIRECTORY);\
@@ -20,15 +21,27 @@ public:
 	ServiceManager() noexcept(false);
 	~ServiceManager() noexcept(false);
 	
-	static BOOL ExtractDriverFromResource();
-	static BOOL DeleteDriverFromDisk();
+	BOOL RegisterService();
+	
 
+	SERVICE_STATUS m_ServiceStatus = { 0 };
+	SERVICE_STATUS_HANDLE m_StatusHandle = NULL;
+	HANDLE m_ServiceStopEvent = INVALID_HANDLE_VALUE;
+	BOOL bRunInBackground = FALSE;
 
 
 private:
+	BOOL ExtractDriverFromResource();
 	BOOL LoadDriver();
+
 	BOOL UnloadDriver();
+	BOOL DeleteDriverFromDisk();
+
 
 	SC_HANDLE hService = NULL;
 	SC_HANDLE hSCManager = NULL;
+
+	BOOL bIsDriverLoaded = FALSE;
+	BOOL bIsDriverExtracted = FALSE;
+
 };

Broker/Session.cpp
@@ -51,8 +51,8 @@ void Session::Start()
 void Session::Stop()
 {
 	m_State = SessionState::Idle;
-	ResetEvent(m_hTerminationEvent);
-	SetEvent(m_hTerminationEvent);
+	::ResetEvent(m_hTerminationEvent);
+	::SetEvent(m_hTerminationEvent);
 }
 
 

Broker/main.h
@@ -9,4 +9,6 @@
 
 #include <iostream>
 
-Session* Sess;
+
+DWORD RunForever(LPVOID lpParameter);
+

Driver/Driver.c
@@ -13,9 +13,7 @@
 UINT16 g_dwNumberOfHandle;
 
 static PDEVICE_OBJECT g_DeviceObject;
-//static IO_REMOVE_LOCK g_DriverRemoveLock;
-//static KSPIN_LOCK g_SpinLock;
-//static KLOCK_QUEUE_HANDLE g_SpinLockQueue;
+
 
 //
 // Not more than one process can interact with the device object
@@ -753,13 +751,13 @@ NTSTATUS _Function_class_(DRIVER_DISPATCH) DriverDeviceControlRoutine(_In_ PDEVI
         break;
 
     default:
-        CfbDbgPrintErr(L"Received invalid ioctl code 0x%X\n", IoctlCode);
+        CfbDbgPrintErr(L"Received invalid ioctl code %#x\n", IoctlCode);
         Status = STATUS_INVALID_DEVICE_REQUEST;
         break;
     }
 
     if (!NT_SUCCESS(Status))
-        CfbDbgPrintErr(L"IOCTL #%x returned %#x\n", IoctlCode, Status);
+        CfbDbgPrintErr(L"IOCTL %#x returned %#x\n", IoctlCode, Status);
 
     
     return CompleteRequest(Irp, Status, dwDataWritten);

commit d2465dfaf532706acc3f949385fbeb908160efa2

[issue #13] fixed bug introduced with 379c50f41d829cce8b147e2ede8d64d1b4d4f933
When running as a service , the session was  not deleted and the driver not unload. This commit fixes this and forces RunForever() to always wait for the stop event from the service controller

Broker/ServiceManager.cpp
@@ -341,10 +341,6 @@ static VOID ServiceCtrlHandler(DWORD dwCtrlCode)
 		if (ServiceManager.m_ServiceStatus.dwCurrentState != SERVICE_RUNNING)
 			break;
 
-		/*
-		 * Perform tasks necessary to stop the service here
-		 */
-
 		ServiceManager.m_ServiceStatus.dwControlsAccepted = 0;
 		ServiceManager.m_ServiceStatus.dwCurrentState = SERVICE_STOP_PENDING;
 		ServiceManager.m_ServiceStatus.dwWin32ExitCode = 0;
@@ -355,10 +351,9 @@ static VOID ServiceCtrlHandler(DWORD dwCtrlCode)
 			xlog(LOG_DEBUG, L"SetServiceStatus() failed");
 			break;
 		}
-
-		// This will signal the worker thread to start shutting down
-		Sess->Stop();
+	
 		::SetEvent(ServiceManager.m_ServiceStopEvent);
+		Sess->Stop();
 		break;
 
 	default:
@@ -451,7 +446,7 @@ static VOID ServiceMain(DWORD argc, LPWSTR* argv)
 		// Let's start CFB in background
 		//
 
-		HANDLE hThread = ::CreateThread(NULL, 0, RunForever, NULL, 0, NULL);
+		HANDLE hThread = ::CreateThread(NULL, 0, RunForever, &ServiceManager.m_ServiceStopEvent, 0, NULL);
 		if (!hThread)
 		{
 			xlog(LOG_ERROR, L"Failed to start the RunForever() thread\n");
@@ -474,6 +469,12 @@ static VOID ServiceMain(DWORD argc, LPWSTR* argv)
 		{
 			xlog(LOG_ERROR, L"SetServiceStatus() failed\n");
 		}
+
+
+		//
+		// Delete the session (i.e. stop the process service and unload the driver)
+		//
+		delete Sess;
 
 	} 
 	while (0);

Broker/main.cc
@@ -204,7 +204,7 @@ Run forever loop, can be run from either the standalone mode or own process serv
 
 Arguments:
 
-	lpParameter - unused parameter, here only for WINAPI compatibility.
+	lpParameter - unused parameter if running in standalone, holds a handle to the stop event if running as a service.
 
 
 Return Value:
@@ -212,14 +212,13 @@ Return Value:
 	Return 0 on success, or the last error code.
 
 --*/
-DWORD RunForever(LPVOID lpParameter)
+DWORD RunForever(_In_ LPVOID lpParameter)
 {
-	UNREFERENCED_PARAMETER(lpParameter);
 
 	DWORD retcode = ERROR_SUCCESS;
-	HANDLE ThreadHandles[3] = { 0 };
+	HANDLE ThreadHandles[4] = { INVALID_HANDLE_VALUE };
+	DWORD dwNumberOfHandles = 3 + ((lpParameter != NULL) ? 1 : 0);
 	DWORD dwWaitResult = 0;
-
 
 	//
 	// Initialize the broker <-> driver thread
@@ -262,13 +261,18 @@ DWORD RunForever(LPVOID lpParameter)
 
 
 	//
-	// Wait for those 2 threads to finish
+	// Wait for the threads to finish
 	//
 	ThreadHandles[0] = Sess->m_hFrontendThread;
 	ThreadHandles[1] = Sess->m_hIrpFetcherThread;
 	ThreadHandles[2] = Sess->m_hBackendThread;
 
-	dwWaitResult = ::WaitForMultipleObjects(_countof(ThreadHandles), ThreadHandles, TRUE, INFINITE);
+	if (lpParameter)
+	{
+		ThreadHandles[3] = *((PHANDLE)lpParameter);
+	}
+
+	dwWaitResult = ::WaitForMultipleObjects(dwNumberOfHandles, ThreadHandles, TRUE, INFINITE);
 
 	switch (dwWaitResult)
 	{
@@ -394,16 +398,15 @@ int wmain(int argc, wchar_t** argv)
 				retcode = EXIT_FAILURE;
 				break;
 			}
+
+			if (RunForever(NULL) != ERROR_SUCCESS)
+			{
+				xlog(LOG_ERROR, L"RunForever() returned with error\n");
+				break;
+			}
+
+			dbg(L"RunForever() finished successfully\n");
 		}
-
-
-		if (RunForever(NULL) != ERROR_SUCCESS)
-		{
-			xlog(LOG_ERROR, L"RunForever() returned with error\n");
-			break;
-		}
-
-		dbg(L"RunForever() finished successfully\n");
 	} 
 	while (0);
 