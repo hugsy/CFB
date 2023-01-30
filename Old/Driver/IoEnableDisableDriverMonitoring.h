9ce602f24917adcf270c53d0bf34642c242aadfc
hugsy <hugsy@blah.cat>
2023-01-29 17:26:35 -0800

    all works in new version, deleting old folder

--- Old/Driver/IoEnableDisableDriverMonitoring.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-
-#include "Common.h"
-#include "Driver.h"
-
-
-NTSTATUS HandleIoEnableDriverMonitoring(_In_ PIRP Irp, _In_ PIO_STACK_LOCATION Stack);
-NTSTATUS HandleIoDisableDriverMonitoring(_In_ PIRP Irp, _In_ PIO_STACK_LOCATION Stack);
